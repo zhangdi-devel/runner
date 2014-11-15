79bc5de65cc055e9459aa7c0df29347303f99545
Di Zhang <zhangdi.devel@gmail.com>
2014-11-15 00:32:50 -0600

    bug fixed

--- src/Vtools/Command.py
+++ src/Vtools/Command.py
@@ -121,7 +121,7 @@ class Compare(Args):
         compare.compare(self)
 
 class Associate(Args):
-    def __init__(self, Variants='', Phenotypes=[], Covariates=[], Var_info=[], Geno_info=[], Geno_name='GT', Methods=[], Group_by=[], Samples=[], Genotypes=[], Discard_samples=[], Discard_variants=[], To_db='', Delimiter=None, Force=False, Jobs=1, Unknown_args=None):
+    def __init__(self, Variants='', Phenotypes=[], Covariates=[], Var_info=[], Geno_info=[], Geno_name='GT', Methods=[], Group_by=[], Samples=[], Genotypes=[], Discard_samples=[], Discard_variants=[], To_db='', Delimiter=None, Force=False, Jobs=1, Unknown_args=[]):
         Args.__init__(self)
         self.variants = Variants
         self.phenotypes = Phenotypes
