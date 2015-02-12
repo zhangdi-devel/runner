b7512483f81ba8f4f32fc2fb46c5a630beec30cd
Di Zhang <zhangdi.devel@gmail.com>
2015-02-12 17:13:49 -0600

    bugfix: Pipeline.init_project

--- src/Vtools/Pipeline.py
+++ src/Vtools/Pipeline.py
@@ -25,7 +25,8 @@ def init_project(runtime_parameters):
                    Format=runtime_parameters.get('vtools', 'format'),
                    Jobs=runtime_parameters.getint('vtools', 'jobs')).Run()
         #import phenotypes
-        rvc.Phenotype(From_file=runtime_parameters.get('vtools', 'pheno')).Run()
+        rvc.Phenotype(From_file=runtime_parameters.get('vtools', 'pheno'),
+                      Delimiter=':').Run()
         
 ##genotype level
 def genotype_level(runtime_parameters):
